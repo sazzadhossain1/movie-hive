126c9d33bf53d9be7fd5de59da8acca11b46a8e3
sazzad tomal <sazzadtomal1@gmail.com>
2025-01-03 00:24:09 +0600

    navigation responsive done

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,7 +1,42 @@
 import React from "react";
 import "./Footer.css";
+import footerBg from "../../accets/FooterPhoto/footerBg.png";
 const Footer = () => {
-  return <div>{/* <h1>This is Footer</h1> */}</div>;
+  return (
+    <div className="footer_parent_div">
+      <div className="footer_grid_div">
+        <div>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+        </div>
+        <div>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+        </div>
+        <div>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+        </div>
+        <div>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+          <p>Lorem, ipsum.</p>
+        </div>
+      </div>
+      <div className="footer_blank_div"></div>
+      <div className="footer_copyright_div">
+        <p>© Movie Hive 2025</p>
+      </div>
+      {/* <img className="footerBg" src={footerBg} alt="" /> */}
+    </div>
+  );
 };
 
 export default Footer;
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -9,13 +9,13 @@ const Home = () => {
   const [isDownloadBtnVisible, setIsDownloadBtnVisible] = useState(false);
 
   const showMovieDownloadBtn = () => {
-    setIsDownloadBtnVisible(true); // Update state to show the download button
+    setIsDownloadBtnVisible(true);
   };
 
   return (
     <div className="home_parent_div">
-      <Banner></Banner>
-      <FirstSection></FirstSection>
+      {/* <Banner></Banner>
+      <FirstSection></FirstSection> */}
       {/* <h1>Welcome Movie Hive</h1>
       <h2>
         Pushpa 2 (2024) UNCUT 1080p HDRip x264 [Dual Audio][Hindi 2.0+Tamil 2.0]
@@ -44,56 +44,6 @@ const Home = () => {
           </button>
         </Link>
       )} */}
-      <p>Lorem ipsum dolor sit amet.</p>
-      <p>Ut ab corrupti hic cupiditate.</p>
-      <p>Veniam dolorem atque beatae dolore?</p>
-      <p>Accusamus maiores dolorum beatae quidem.</p>
-      <p>Soluta vero quis pariatur cupiditate.</p>
-      <p>Tenetur laudantium maxime quam. Quidem!</p>
-      <p>Nihil accusamus odit quia amet?</p>
-      <p>Sunt nesciunt error mollitia suscipit.</p>
-      <p>Architecto earum odit quae numquam.</p>
-      <p>Molestias magnam reprehenderit sed numquam?</p>
-      <p>Id cupiditate numquam perspiciatis totam?</p>
-      <p>Reiciendis aliquam libero autem ipsa?</p>
-      <p>Eos atque facere animi reprehenderit?</p>
-      <p>Odit ratione velit corrupti debitis?</p>
-      <p>Cupiditate obcaecati ab repudiandae minima.</p>
-      <p>Facere repudiandae earum error voluptatibus?</p>
-      <p>Nam praesentium ratione qui temporibus.</p>
-      <p>Velit assumenda unde qui dolore.</p>
-      <p>Molestiae dolor voluptates ratione laboriosam.</p>
-      <p>Doloribus cum dignissimos distinctio nostrum.</p>
-      <p>Similique quis nesciunt dolore sint!</p>
-      <p>Fugit asperiores quidem omnis esse?</p>
-      <p>Doloribus aperiam inventore facere quo.</p>
-      <p>Natus nostrum blanditiis eius? Tempore.</p>
-      <p>Enim distinctio sed odit similique.</p>
-      <p>Placeat illo quod commodi? Repellat!</p>
-      <p>Ut eveniet molestias sed nesciunt.</p>
-      <p>Accusamus dolorum laboriosam eveniet obcaecati!</p>
-      <p>Omnis dolorem culpa commodi debitis.</p>
-      <p>Pariatur consequatur voluptas dolor magni!</p>
-      <p>Illo eum iusto modi neque!</p>
-      <p>Repellat veniam molestias enim voluptates.</p>
-      <p>Ut non adipisci voluptatibus blanditiis?</p>
-      <p>A illum optio accusantium dolor.</p>
-      <p>Minima alias rem error earum?</p>
-      <p>Ea soluta ipsa iusto aut.</p>
-      <p>Officia alias repellendus blanditiis commodi.</p>
-      <p>Dolorum suscipit repellendus debitis possimus.</p>
-      <p>Voluptate obcaecati non repellendus perferendis!</p>
-      <p>Dolore voluptate hic ipsum alias.</p>
-      <p>Omnis sint placeat natus dolor!</p>
-      <p>Temporibus, doloribus atque. Ipsa, optio.</p>
-      <p>Libero cum cumque ullam et.</p>
-      <p>Maiores esse neque natus adipisci.</p>
-      <p>Obcaecati voluptas vitae impedit atque?</p>
-      <p>Ipsam esse quibusdam vel cupiditate?</p>
-      <p>Est blanditiis illum facere repudiandae.</p>
-      <p>Necessitatibus placeat eaque magnam reprehenderit.</p>
-      <p>Saepe deserunt labore quia tempora.</p>
-      <p>Est earum aperiam officia corporis!</p>
     </div>
   );
 };
